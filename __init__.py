bdecd02815ea560e65c08a844ed4626c557ee248
Jorge R <rodriguezrevellojj@gmail.com>
2022-07-28 14:06:52 +0200

    Initial init with version, author and group

---
+++ __init__.py
@@ -0,0 +1,18 @@
+__version__ = "0.0.1"
+__author__ = "Jorge J. Rodriguez Revello <rodriguezrevellojj@uma.es>"
+__group__ = "Khaos Research <khaos.uma.es>"
+
+HEADER = "\n".join(
+    [
+
+        r"                                             ",
+        r"                   \/  \/                    ",
+        r"                                             ",
+        "                                                ",
+        f" ver. {__version__}     group  {__group__}     ",
+        f"author {__author__}                            ",
+        "                                                ",                            
+    ]
+
+) 
+print(HEADER)
