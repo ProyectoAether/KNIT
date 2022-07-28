f7728633eac0922733f46e3dcf52e508a8eab010
Jorge R <rodriguezrevellojj@gmail.com>
2022-07-28 14:15:02 +0200

    Initial structure, main and requirements

---
+++ knitbio/first_layer.py
@@ -0,0 +1,30 @@
+
+
+def knit(
+        sparql_service: str,
+        REST_URL: str,
+        list_text: list,
+        API_KEY: str,
+        IP_SERVER_NEO4J: str,
+        USER_NEO4J: str,
+        PASSWORD_NEO4J: str,
+        wc: str,
+        wa: str,
+        wd: str,
+        ws: str,
+        ONTOLOGY: list,
+        ontology_denied=list
+):
+    print(sparql_service)
+    print(REST_URL)
+    print(list_text)
+    print(API_KEY)
+    print(IP_SERVER_NEO4J)
+    print(USER_NEO4J)
+    print(PASSWORD_NEO4J)
+    print(wc)
+    print(wa)
+    print(wd)
+    print(ws)
+    print(ONTOLOGY)
+    print(ontology_denied)
---
+++ main.py
@@ -0,0 +1,80 @@
+import __init__
+import pandas as pd
+from decouple import config
+from knitbio.first_layer import knit
+
+print(config("API_KEY"))
+
+
+def star_knit(
+    sparql_service,
+    REST_URL: str,
+    path_csv_file: str,
+    sep_csv: str,
+    wc: str,
+    wa: str,
+    wd: str,
+    ws: str,
+    ONTOLOGY: list,
+    ontology_denied: list,
+):
+
+    df = pd.read_csv(path_csv_file, sep=sep_csv, dtype="str")
+    list_text = []
+    for row in df:
+        list_text.append(row.upper().replace('"', ""))
+
+    while len(list_text) >= 100:
+
+        knit(
+            sparql_service,
+            REST_URL,
+            list_text[0:100],
+            (config("API_KEY")),
+            (config("IP_SERVER_NEO4J")),
+            (config("USER_NEO4J")),
+            (config("PASSWORD_NEO4J")),
+            wc,
+            wa,
+            wd,
+            ws,
+            ONTOLOGY,
+            ontology_denied,
+        )
+        for element in list_text[0:100]:
+            list_text.remove(element)
+    else:
+
+        knit(
+            sparql_service,
+            REST_URL,
+            list_text,
+            (config("API_KEY")),
+            (config("IP_SERVER_NEO4J")),
+            (config("USER_NEO4J")),
+            (config("PASSWORD_NEO4J")),
+            wc,
+            wa,
+            wd,
+            ws,
+            ONTOLOGY,
+            ontology_denied,
+        )
+
+
+star_knit(
+    (config("sparql_service")),
+    (config("REST_URL")),
+    (config("path_csv")),
+    (config("sep_csv")),
+    (config("wc")),
+    (config("wa")),
+    (config("wd")),
+    (config("ws")),
+    (config("ontology_list")),
+    (config("ontology_denied")),
+)
+
+print("end_!")
+
+
